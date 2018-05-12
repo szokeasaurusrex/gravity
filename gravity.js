ee55bc8222be2ca77b3d5965586512f03b782d39
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2018-05-12 18:01:38 -0400

    Collisions working partially

--- gravity.js
+++ gravity.js
@@ -58,6 +58,7 @@ function Planet(id, mass, radius, position, velocity, fixed) {
   this.position = position;
   this.velocity = velocity;
   this.fixed = fixed;
+  this.collided_planets = [];
 }
 
 Planet.prototype.draw = function(ctx) {
@@ -66,7 +67,7 @@ Planet.prototype.draw = function(ctx) {
   ctx.stroke();
 };
 
-Planet.prototype.updateAccel = function(planets) {
+Planet.prototype.updateAccel = function(ctx, planets) {
   // planets is an array containing all other planets
   this.accel = new Vector (0, 0);
   if (this.fixed === false) {
@@ -76,27 +77,63 @@ Planet.prototype.updateAccel = function(planets) {
         var accel_magnitude = gravitational_constant * planets[i].mass / Math.pow(distance_vector.magnitude(), 2);
         var accel_change = distance_vector.unitize().scale(accel_magnitude);
         this.accel = this.accel.add(accel_change);
-        console.log(this.accel);
+
+        var xdiff = this.position.add(planets[i].position.scale(-1));
+        if (xdiff.magnitude() <= this.radius + planets[i].radius && this.collided_planets.indexOf(planets[i].id) == -1) {
+          // collisions with other planets
+          var vdiff = this.velocity.add(planets[i].velocity.scale(-1));
+          var m1 = this.mass;
+          var m2 = planets[i].mass;
+          var accel_change = xdiff.scale(-2 * m2 / (m1 + m2) * vdiff.dot(xdiff) / Math.pow (xdiff.magnitude(), 2));
+          this.accel = this.accel.add(accel_change);
+          this.collided_planets.push(planets[i].id);
+        }
+        else if (xdiff.magnitude() >= this.radius + planets[i].radius && this.collided_planets.indexOf(planets[i].id) != -1){
+          var index = this.collided_planets.indexOf(planets[i].id);
+          this.collided_planets.splice(index, 1);
+        }
       }
     }
-    // TODO add acceleration change for collisions
+    if ((this.position.x < this.radius && this.velocity.x < 0) || (this.position.x > ctx.canvas.width - this.radius && this.velocity.x > 0)) {
+      // collisions with left and right walls
+      this.accel.x -= 2 * this.velocity.x;
+    }
+    if ((this.position.y < this.radius && this.velocity.y < 0) || (this.position.y > ctx.canvas.height - this.radius && this.velocity.y > 0)) {
+      // collisions with top and bottom walls
+      this.accel.y -= 2 * this.velocity.y;
+    }
   }
 }
 
-Planet.prototype.updatePosition = function() {
+Planet.prototype.updatePosition = function(planets) {
   if (this.fixed === false) {
     this.velocity = this.velocity.add(this.accel);
     this.position = this.position.add(this.velocity);
+    this.position_shift = new Vector(0, 0);
+    for (var i = 0; i < planets.length; i++) {
+      // collision shifting
+      var xdiff = this.position.add(planets[i].position.scale(-1));
+      var distance = xdiff.magnitude() - (this.radius + planets[i].radius);
+      if (this.id != planets[i].id && distance <= 0) {
+        if (planets[i].fixed === true) {
+          this.position_shift = this.position_shift.add(xdiff.unitize().scale(-distance));
+        }
+        else {
+          this.position_shift = this.position_shift.add(xdiff.unitize().scale(-distance/2));
+        }
+      }
+    }
+    this.position = this.position.add(this.position_shift);
   }
 }
 
 function animate (ctx, planets) {
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
   for (var i = 0; i < planets.length; i++) {
-    planets[i].updateAccel(planets);
+    planets[i].updateAccel(ctx,planets);
   }
   for (var i = 0; i < planets.length; i++) {
-    planets[i].updatePosition();
+    planets[i].updatePosition(planets);
     planets[i].draw(ctx);
   }
   animationId = window.requestAnimationFrame(function() {animate(ctx, planets);});
